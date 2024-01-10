b07b974bf2a18bf867c39656a5e538b5b7be86c4
yanan liu <yanan926@aliyun.com>
2024-01-09 17:30:15 -0800

    adjust mobile sytle

--- src/Components/ImageModal.jsx
+++ src/Components/ImageModal.jsx
@@ -1,35 +1,37 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '60%',
-  bgcolor: 'background.paper',
-  border: '1px solid #000',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "85%",
+  bgcolor: "background.paper",
+  border: "1px solid #000",
   boxShadow: 24,
-  p: 4,
-  display: 'flex',
-  flexDirection: 'column',
+  p: 1,
+  display: "flex",
+  flexDirection: "column",
 };
 
-export default function ImageModal({open, handleClose, image}) {
+export default function ImageModal({ open, handleClose, image }) {
   return (
-      <Modal
-        open={open}
-        onClose={handleClose}
-      >
-        <Box sx={style}>
-        <Button onClick={handleClose} sx={{alignSelf:'flex-end'}}>Close</Button>
-        <Box> <img src={image} alt={'travel image'} 
-        style={{width: '100%', height:'500px', objectFit: 'cover'}}/></Box>
-       
-        </Box>
-      </Modal>
+    <Modal open={open} onClose={handleClose}>
+      <Box sx={style}>
+        {" "}
+        <Button onClick={handleClose} sx={{ alignSelf: "flex-end" }} size="small">
+          Close
+        </Button>
+        <img
+          src={image}
+          alt={"travel image"}
+          style={{ width: "100%", height: "85vh", objectFit: "cover" }}
+        />
+      </Box>
+    </Modal>
   );
 }
